1d6c6eed0bccd99541f174881c99f4a544314894
wolfpn <efootballdomelhor@gmail.com>
2025-12-27 00:11:51 +0300

    script.js

--- script.js
+++ script.js
@@ -1,41 +1,29 @@
-const canvas = document.getElementById("snow");
-const ctx = canvas.getContext("2d");
+const c = document.getElementById("snow");
+const ctx = c.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let snow = [];
-
-for (let i = 0; i < 120; i++) {
-  snow.push({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    r: Math.random() * 3 + 1,
-    d: Math.random() + 1
-  });
+function resize() {
+  c.width = innerWidth;
+  c.height = innerHeight;
 }
+resize();
+addEventListener("resize", resize);
+
+let snow = Array.from({length: 120}, () => ({
+  x: Math.random()*c.width,
+  y: Math.random()*c.height,
+  r: Math.random()*3+1,
+  d: Math.random()+1
+}));
 
 function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "white";
-  ctx.beginPath();
-  for (let i = 0; i < snow.length; i++) {
-    let s = snow[i];
-    ctx.moveTo(s.x, s.y);
-    ctx.arc(s.x, s.y, s.r, 0, Math.PI * 2);
-  }
-  ctx.fill();
-  update();
+  ctx.clearRect(0,0,c.width,c.height);
+  ctx.fillStyle="white";
+  snow.forEach(s=>{
+    ctx.beginPath();
+    ctx.arc(s.x,s.y,s.r,0,Math.PI*2);
+    ctx.fill();
+    s.y+=s.d;
+    if(s.y>c.height) s.y=0;
+  });
 }
-
-function update() {
-  for (let i = 0; i < snow.length; i++) {
-    let s = snow[i];
-    s.y += s.d;
-    if (s.y > canvas.height) {
-      snow[i] = { x: Math.random()*canvas.width, y: 0, r: s.r, d: s.d };
-    }
-  }
-}
-
-setInterval(draw, 30);
+setInterval(draw,30);

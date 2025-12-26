3496258d1b16fa366f2fc6675e6f8b4123f7c9ca
wolfpn <efootballdomelhor@gmail.com>
2025-12-26 23:09:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const elements = document.querySelectorAll('.animate');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, { threshold: 0.2 });
+
+elements.forEach(el => observer.observe(el));
